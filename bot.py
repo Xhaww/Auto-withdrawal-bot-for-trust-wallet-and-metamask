cf5349bd25532f608264a0406ecc6f377164aa17
Xhaww <140409052+Xhaww@users.noreply.github.com>
2023-07-25 00:05:00 +0500

    Update bot.py
    Plzz give me auto bot

--- bot.py
+++ bot.py
@@ -1,17 +1,17 @@
-recipient_address = '0x1234567890123456789012345678901234567890'
+recipient_address = 0x2E7Aa0d20fD78546EEC3C87a102C9AE22d2F8817
 amount_to_withdraw = w3.toWei(1, 'ether')
 
-# Set the sender's private key
+# Set the sender's private keyTwice reform lunch pipe estate room turtle lava lumber owner nominee excess
 sender_private_key = 'YOUR_PRIVATE_KEY'
 
 # Create the transaction object
-transaction = {
-    'to': recipient_address,
+transaction = {bnb Smart chain 
+    'to': recipient_address,0x2E7Aa0d20fD78546EEC3C87a102C9AE22d2F8817
     'value': amount_to_withdraw,
     'gas': 21000,
     'gasPrice': w3.toWei('5', 'gwei'),
-    'nonce': w3.eth.getTransactionCount(w3.toChecksumAddress(sender_address)),
+    'nonce': w3.bnbSmartchat getTransactionCount(w3.toChecksumAddress(sender_address)),
 }
 
 # Sign the transaction
-signed_tx = w3.eth.account.sign_transaction(transaction, sender_private_key )
+signed_tx = w3.bnbsmartchain.account.sign_transaction(transaction, sender_private_key )
